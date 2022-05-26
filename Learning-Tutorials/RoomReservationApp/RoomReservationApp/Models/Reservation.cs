53f5987d2031f70c101dda2eba76b3fdc42fc92a
E_Nagy <Edina_Nagy@sluz.ch>
2022-05-26 18:13:54 +0200

    Starting with Tutorial Models

---
+++ Learning-Tutorials/RoomReservationApp/RoomReservationApp/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomReservationApp.Models
+{
+    internal class Reservation
+    {
+        
+        public RoomID RoomID { get;}
+        public DateTime ReservationStart { get;}
+        public DateTime ReservationEnd { get;}
+
+        public Reservation(RoomID roomID, DateTime reservationStart, DateTime reservationEnd)
+        {
+            RoomID = roomID;
+            ReservationStart = reservationStart;
+            ReservationEnd = reservationEnd;
+        }
+
+    }
+}
---
+++ Learning-Tutorials/RoomReservationApp/RoomReservationApp/Models/ReservationBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomReservationApp.Models
+{
+    public class ReservationBook
+    {
+
+        private readonly Dictionary<RoomID, List<Reservation>> roomsToReservations;
+
+        public ReservationBook()
+        {
+            roomsToReservations = new Dictionary<RoomID, List<Reservation>>();
+        }
+
+    }
+}
